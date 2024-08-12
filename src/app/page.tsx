8ef14d3c6dc9b98657de3a0e66655811e37c1c93
Nitish Singh <nitishsingh925@gmail.com>
2024-08-12 16:26:03 +0530

    base with shadcn ui

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen">
+      <Button>hi</Button>
+    </main>
+  );
+}
