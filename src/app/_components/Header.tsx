c332626d19113bec5748481b21500d16c2b56a43
Nitish Singh <nitishsingh925@gmail.com>
2024-08-13 14:52:24 +0530

    add DropdownMenu

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -2,6 +2,14 @@ import { Button } from "@/components/ui/button";
 import { LayoutGrid, Search, ShoppingBag } from "lucide-react";
 import Image from "next/image";
 import React, { FC } from "react";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 const Header: FC = () => {
   return (
@@ -9,10 +17,23 @@ const Header: FC = () => {
       {/* left side logo category search */}
       <div className="flex items-center gap-8">
         <Image src="/logo.png" alt="logo" width={150} height={100} />
-        <h2 className="hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200">
-          <LayoutGrid className="h-5 w-5" />
-          Category
-        </h2>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="outline-none">
+            <h2 className="hidden md:flex gap-2 items-center border rounded-full p-2 px-10 bg-slate-200 cursor-pointer">
+              <LayoutGrid className="h-5 w-5" />
+              Category
+            </h2>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>Profile</DropdownMenuItem>
+            <DropdownMenuItem>Billing</DropdownMenuItem>
+            <DropdownMenuItem>Team</DropdownMenuItem>
+            <DropdownMenuItem>Subscription</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <div className="hidden md:flex gap-3 items-center border rounded-full p-2 px-5">
           <Search />
           <input type="text" placeholder="Search" className="outline-none" />
