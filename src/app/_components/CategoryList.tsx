e3b75b7611521ddc205b2f421acbc8f9560a5916
Nitish Singh <nitishsingh925@gmail.com>
2024-08-15 15:59:27 +0530

    CategoryList with db collection

---
+++ src/app/_components/CategoryList.tsx
@@ -0,0 +1,45 @@
+"use client";
+import React from "react";
+import Image from "next/image";
+import useCategories from "@/hooks/useCategories";
+
+const CategoryList = () => {
+  const { categories, loading, error } = useCategories();
+
+  if (loading) {
+    return <div className="text-center py-4">Loading...</div>;
+  }
+
+  if (error) {
+    return <div className="text-red-500">{error}</div>;
+  }
+
+  return (
+    <div className="p-5">
+      <h2 className="text-green-600 font-bold text-2xl">Shop by Category</h2>
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-7 gap-5 mt-4">
+        {categories.map((category) => (
+          <div
+            key={category._id}
+            className="group flex flex-col items-center bg-green-50 p-3 rounded-lg cursor-pointer hover:bg-green-200 transition-all duration-200 ease-in-out"
+          >
+            <div className="relative w-14 h-14">
+              <Image
+                src={`/${category.icon.url}`}
+                alt={category.name}
+                layout="fill"
+                objectFit="contain"
+                className="rounded-full transition-transform duration-300 group-hover:scale-110"
+              />
+            </div>
+            <span className="text-green-800 font-semibold mt-2 text-sm lg:text-base group-hover:text-green-900">
+              {category.name}
+            </span>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default CategoryList;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import CategoryList from "./_components/CategoryList";
 import Slider from "./_components/Slider";
 
 export default function Home() {
   return (
     <main className="">
       <Slider />
+      <CategoryList />
     </main>
   );
 }
