ce9afdb373574d41a05d00282d6ba4a147f9cbd5
Nitish Singh <nitishsingh925@gmail.com>
2024-08-20 16:06:44 +0530

    bug fix of db

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,21 +2,19 @@ import type { Metadata } from "next";
 import { Outfit } from "next/font/google";
 import "./globals.css";
 import Header from "./_components/Header";
-import dbConnect from "@/lib/dbConnect";
 
 const outfit = Outfit({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "E-Shop",
-  description: "this app create By Nitish Singh",
+  description: "This app created by Nitish Singh",
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-  dbConnect: () => Promise<void>;
-}>) {
+}) {
   return (
     <html lang="en">
       <body className={outfit.className}>
