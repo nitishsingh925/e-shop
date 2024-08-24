05d8d6b4f3640be48a03e9f69b750c71e05d282a
Nitish Singh <nitishsingh925@gmail.com>
2024-08-24 23:28:20 +0530

    improve style and code

--- src/app/_components/Header.tsx
+++ src/app/_components/Header.tsx
@@ -9,7 +9,7 @@ const Header: FC = () => {
   return (
     <header className="flex px-2 sm:px-3 md:px-4 lg:px-6 xl:px-8 2xl:px-10 shadow-md items-center justify-between sticky top-0 z-20 backdrop-blur-lg">
       <div className="flex items-center gap-8">
-        <Image src="/logo.png" alt="logo" width={150} height={100} priority />
+        <Image src="/logo.png" alt="logo" width={100} height={60} priority />
         <DropdownMenuComponent />
         <div className="hidden md:flex gap-3 items-center border rounded-full p-2 px-5">
           <Search aria-label="Search Icon" />
