c0747aa2f10f4e3cd6361bd355e90a92959624eb
Nitish Singh <nitishsingh925@gmail.com>
2024-09-03 14:04:30 +0530

    add skeleton

--- src/components/Slider.tsx
+++ src/components/Slider.tsx
@@ -8,6 +8,7 @@ import {
   CarouselPrevious,
 } from "@/components/ui/carousel";
 import Image from "next/image";
+import { Skeleton } from "./ui/skeleton";
 
 // Define the type for banner data
 export interface Banner {
@@ -19,6 +20,7 @@ export interface Banner {
 
 const Slider: FC = () => {
   const [banners, setBanners] = useState<Banner[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(true); // State to manage loading state
 
   useEffect(() => {
     const fetchBanners = async () => {
@@ -26,10 +28,11 @@ const Slider: FC = () => {
         const response = await fetch("api/banner?type=top");
         const data: Banner[] = await response.json();
         console.log(data);
-
         setBanners(data);
+        setIsLoading(false); // Set loading to false once data is fetched
       } catch (error) {
         console.error("Failed to fetch banners:", error);
+        setIsLoading(false); // Set loading to false on error as well
       }
     };
 
@@ -40,17 +43,27 @@ const Slider: FC = () => {
     <div className="py-10">
       <Carousel>
         <CarouselContent>
-          {banners.map((banner) => (
-            <CarouselItem key={banner._id}>
-              <Image
-                src={`/${banner.image}`}
-                alt={banner.name}
-                width={1000}
-                height={400}
-                className="w-full h-[200px] sm:h-[300px] md:h-[350px] lg:h-[400px] object-cover rounded-2xl"
-              />
-            </CarouselItem>
-          ))}
+          {isLoading ? (
+            // Display "Coming Soon" while loading
+            <>
+              <CarouselItem>
+                <Skeleton className="w-full h-[200px] sm:h-[300px] md:h-[350px] lg:h-[400px] flex items-center justify-center rounded-2xl bg-gradient-to-r from-neutral-300 to-neutral-400 animate-pulse shadow-md"></Skeleton>
+              </CarouselItem>
+            </>
+          ) : (
+            // Display banners once data is loaded
+            banners.map((banner) => (
+              <CarouselItem key={banner._id}>
+                <Image
+                  src={`/${banner.image}`}
+                  alt={banner.name}
+                  width={1000}
+                  height={400}
+                  className="w-full h-[200px] sm:h-[300px] md:h-[350px] lg:h-[400px] object-cover rounded-2xl"
+                />
+              </CarouselItem>
+            ))
+          )}
         </CarouselContent>
         <CarouselPrevious className="absolute top-1/2 -translate-y-1/2 left-5" />
         <CarouselNext className="absolute top-1/2 -translate-y-1/2 right-5" />
